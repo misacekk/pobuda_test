54ec26ef48920984ed6002fe4f2fe2640c91fc38
pobmic15 <pobmic15@gasos-ro.cz>
2026-01-15 12:17:44 +0100

    e

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,62 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
 
-        Uzivatel uzivatel = new Uzivatel();
+        Uzivatel uzivatel = new Uzivatel("j5a. n", "Pavel", "21.2.2009", "belsky@gasos-ro.cz", "kgajgoajga2252");
 
-        /*StringBuilder sb = new StringBuilder("novjar187@firma.cz");
-        for (int i = 0; i < sb.length(); i++) {
-            if (sb.charAt(i) == '@' && sb.charAt(i) == '.') {
-                System.out.println("je to mail");
-            }else {
-                System.out.println("neni to mail");
-            }*/
-        StringBuilder sb = new StringBuilder("j5a. n");
-        for (int i = 0; i < sb.length(); i++) {
-            if (Character.isDigit(sb.charAt(i))) {
-                sb.deleteCharAt(i);
-            } else if (sb.charAt(i) == '.') {
-                sb.deleteCharAt(i);
-            } else if (Character.isWhitespace(sb.charAt(i))) {
-                sb.deleteCharAt(i);
+        //Úkol - Příjmení
+            /*StringBuilder sb = new StringBuilder("j5a.n");
+            for (int i = 0; i < sb.length(); i++) {
+                if (Character.isDigit(sb.charAt(i)) || Character.isWhitespace(sb.charAt(i)) || sb.charAt(i) == '.') {
+                    sb.deleteCharAt(i);
+                    sb.setCharAt(0, Character.toUpperCase(sb.charAt(0)));
+                }
             }
-        }
-        System.out.println(sb.toString());
+            System.out.println(sb.toString());*/
+
+
+        //Úkol - Náhodná adresa
+            /*StringBuilder sb = new StringBuilder("Jaroslav Novák");
+            String vysledek = "";
+            for (int i = 0; i < sb.length(); i++) {
+                if (Character.isWhitespace(sb.charAt(i))) {
+                    sb.deleteCharAt(i);
+                    sb.append("187@firma.cz");
+                    vysledek = sb.toString();
+                }
+            }
+            System.out.println(vysledek.toLowerCase());*/
+
+
+        //Úkol - Jméno
+            /*StringBuilder sb = new StringBuilder("Jan Pavel Kryštof");
+            for (int i = 0; i < sb.length(); i++) {
+                if (Character.isDigit(sb.charAt(i)) || Character.isWhitespace(sb.charAt(i)) || sb.charAt(i) == '.') {
+                    sb.deleteCharAt(i);
+                    sb.setCharAt(0, Character.toUpperCase(sb.charAt(0)));
+                }
+            }
+            System.out.println(sb.toString());*/
+
+
+        //Úkol - Zatření Emailu
+            /*StringBuilder sb = new StringBuilder("belsky.gasos-ro.cz");
+            sb.replace(4, 14, "************");
+            System.out.println(sb.toString());*/
+
+
+        //Úkol - Je to Email?
+            /*StringBuilder sb = new StringBuilder("novjar187@firma.cz");
+            Boolean vysledek = false;
+            for (int i = 0; i < sb.length(); i++) {
+                if (sb.equals("@") || sb.equals(".")) {
+                    vysledek = true;
+                } else {
+                    vysledek = false;
+                }
+            }
+            System.out.println(vysledek);*/
 
 
     }
--- src/Uzivatel.java
+++ src/Uzivatel.java
@@ -5,22 +5,105 @@ public class Uzivatel {
     private String email;
     private String heslo;
 
-    public void upravaPrijemni(){
-        StringBuilder sb = new StringBuilder("j5a. n");
+    public Uzivatel(String jmeno, String prijmeni, String datumNarozeni, String email, String heslo) {
+        this.jmeno = jmeno;
+        this.prijmeni = prijmeni;
+        this.datumNarozeni = datumNarozeni;
+        this.email = email;
+        this.heslo = heslo;
+    }
+
+    public void upravaPrijemni() {
+        StringBuilder sb = new StringBuilder("j5a.n");
         for (int i = 0; i < sb.length(); i++) {
-            if (Character.isDigit(sb.charAt(i))) {
+            if (Character.isDigit(sb.charAt(i)) || Character.isWhitespace(sb.charAt(i)) || sb.charAt(i) == '.') {
                 sb.deleteCharAt(i);
-            } else if (sb.charAt(i) == '.') {
-                sb.deleteCharAt(i);
-            } else if (Character.isWhitespace(sb.charAt(i))) {
-                sb.deleteCharAt(i);
+                sb.setCharAt(0, Character.toUpperCase(sb.charAt(0)));
             }
         }
         System.out.println(sb.toString());
     }
-    public void zastreniEmail(){
+
+    public void generaceEmail() {
+        StringBuilder sb = new StringBuilder("Jaroslav Novák");
+        String vysledek = "";
+        for (int i = 0; i < sb.length(); i++) {
+            if (Character.isWhitespace(sb.charAt(i))) {
+                sb.deleteCharAt(i);
+                sb.append("187@firma.cz");
+                vysledek = sb.toString();
+            }
+        }
+        System.out.println(vysledek.toLowerCase());
+    }
+
+    public void upravaJmena() {
+        StringBuilder sb = new StringBuilder("Jan Pavel Kryštof");
+        for (int i = 0; i < sb.length(); i++) {
+            if (Character.isDigit(sb.charAt(i)) || Character.isWhitespace(sb.charAt(i)) || sb.charAt(i) == '.') {
+                sb.deleteCharAt(i);
+                sb.setCharAt(0, Character.toUpperCase(sb.charAt(0)));
+            }
+        }
+        System.out.println(sb.toString());
+    }
+
+    public void zastreniEmail() {
         StringBuilder sb = new StringBuilder("belsky.gasos-ro.cz");
         sb.replace(4, 14, "************");
         System.out.println(sb.toString());
+    }
+
+    public void kontrolaEmail() {
+        StringBuilder sb = new StringBuilder("novjar187@firma.cz");
+        Boolean vysledek = false;
+        for (int i = 0; i < sb.length(); i++) {
+            if (sb.equals("@") || sb.equals(".")) {
+                vysledek = true;
+            } else {
+                vysledek = false;
+            }
+        }
+        System.out.println(vysledek);
+    }
+
+    public String getJmeno() {
+        return jmeno;
+    }
+
+    public String getPrijmeni() {
+        return prijmeni;
+    }
+
+    public String getDatumNarozeni() {
+        return datumNarozeni;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getHeslo() {
+        return heslo;
+    }
+
+    public void setJmeno(String jmeno) {
+        this.jmeno = jmeno;
+    }
+
+    public void setPrijmeni(String prijmeni) {
+        this.prijmeni = prijmeni;
+    }
+
+    public void setDatumNarozeni(String datumNarozeni) {
+        this.datumNarozeni = datumNarozeni;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setHeslo(String heslo) {
+        this.heslo = heslo;
     }
 }
